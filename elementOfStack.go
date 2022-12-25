1a44947d0cfdf9c43dc7bffde0576736dc078f02
Ivan <lavr.i-n@mail.ru>
2022-12-25 18:43:39 +0300

    some messege

--- elementOfStack.go
+++ elementOfStack.go
@@ -5,3 +5,5 @@ type ElementOfStack struct {
 	pointerOnThisElement *ElementOfStack // this field of elemnet contains pointer on itself (self adderess)
 	pointerOnPtiviousElement *ElementOfStack // this field points on privious element in stack
 }
+
+// comment
