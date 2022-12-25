0cf469242e59b1df9c1ef679f74ea55f6a398909
NikolayMorgachyow <morgachyow@inbox.ru>
2022-12-25 22:06:20 +0300

    second commit

--- element.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type Element struct {
-	value int // value of current element
-	pointerOnThisElement *Element // this field of elemnet contains pointer on itself (self adderess)
-	pointerOnPtiviousElement *Element // this field points on privious element in stack
-}
-
-// changes in new branch...
