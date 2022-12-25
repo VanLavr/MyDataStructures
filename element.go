d32dba2ea28b0af298d67d66af3bb0cdf8e0328f
Ivan <86853543+VanLavr@users.noreply.github.com>
2022-12-25 18:40:58 +0300

    Delete element.go

--- element.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type ElementOfStack struct {
-	value int // value of current element
-	pointerOnThisElement *ElementOfStack // this field of elemnet contains pointer on itself (self adderess)
-	pointerOnPtiviousElement *ElementOfStack // this field points on privious element in stack
-}
